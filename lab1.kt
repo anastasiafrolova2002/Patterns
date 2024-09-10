64fd847f27b8a47d2d10ebdc89f1122ffb165ee8
anastasiafrolova2002 <147087434+anastasiafrolova2002@users.noreply.github.com>
2024-09-10 09:20:35 +0300

    LAB_1 Task_2

--- lab1.kt
+++ lab1.kt
@@ -1,4 +1,4 @@
-// создаем класс Student
+//1 создаем класс Student
 class Student(
     val id: Int,
     var firstName: String,
@@ -13,4 +13,13 @@ class Student(
     fun getFullName() = "$lastName $firstName ${middleName ?: ""}"
 
     override fun toString() = "Student(id=$id, fullName='${getFullName()}', phone=$phone, telegram=$telegram, email=$email, github=$github)"
+}
+
+//2 созданием объекты и выводим информацию о них:
+fun main() {
+    val student1 = Student(1, "Ivan", "Ivanov", "Ivanovich", "123456789", "ivanivan", "ivan.ivan@mail.com", "ivan_2002")
+    val student2 = Student(2, "Oleg", "Kitov", null, null, null, "o.kit@mail.com", "o_kit_000")
+
+    println(student1)
+    println(student2)
 }
