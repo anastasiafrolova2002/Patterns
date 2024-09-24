7e9fb680a110607b74944f1d5b1deaf4f7e5e5f1
anastasiafrolova2002 <147087434+anastasiafrolova2002@users.noreply.github.com>
2024-09-24 12:01:03 +0300

    задание 2 - создаем объекты класса, переопределяем метод toString

--- Student_lab_1.kt
+++ Student_lab_1.kt
@@ -44,4 +44,64 @@ class Student (
         set(value) {
             field = value
         }
+        override fun toString(): String {
+            var str = "[ID $ID] $surname $name $secondname"
+            if (phone != null) str += "\nНомер телефона: $phone"
+            if (telegram != null) str += "\nTelegram: $telegram"
+            if (email != null) str += "\nEmail: $email"
+            if (git != null) str += "\nGit: $git"
+            return "$str\n"
+        }
+        fun show() = println(this.toString())
     }
+
+    
+fun main() {
+    val students = mutableListOf<Student>()
+    students.add(
+        Student(
+            1,
+            "Ivan",
+            "Ivanov",
+            "Ivanovich"
+        )
+    )
+    students.add(
+        Student(
+            2,
+            "Jack",
+            "Jacsov",
+            "Jacsovich",
+            "+79528459854",
+            "@chips_the_unlimited",
+            "jack999@mail.com",
+            "https://github.com/jack999")
+    )
+    students.add(
+        Student(
+            3,
+            "Ann",
+            "Anny",
+            "Annovich",
+            telegram="@the_anna",
+            git="https://github.com/ann3000"))
+    students.add(
+        Student(
+            4,
+            "Anastasia",
+            "Frolova",
+            "Alexsandrovna",
+            git="https://gitlab.com/anastasiafrolova2002"
+        )
+    )
+    students.add(
+        Student(
+            5,
+            "Maria",
+            "Pavlogradskaya",
+            "Alexsandrovna",
+            email="maria2003@mail.com"
+        )
+    )
+    students.forEach { it.show() }
+}
