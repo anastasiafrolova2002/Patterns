42cdeaf0010cd19eec01bcf7e67e976b998e908c
anastasiafrolova2002 <147087434+anastasiafrolova2002@users.noreply.github.com>
2024-11-12 11:53:10 +0300

    LAB2 TASK 5 - изменения для абст класса

--- StudentShort.kt
+++ StudentShort.kt
@@ -1,37 +1,35 @@
-class StudentShort {
+class StudentShort : SudentAbst {
     companion object {
-        private val surnameAndInitialsRegex = Regex("""^[\p{L}-]+ \p{L}\.( \p{L}\.)?$""")
-        private val gitRegex = Regex("""^(https?://)?([A-Za-z0-9]+\.)?[A-Za-z0-9]+\.[A-Za-z0-9]+/[A-Za-z0-9_-]+/?$""")
-        private val phoneRegex = Regex("""^\+?[0-9]{10,15}$""")
-        private val telegramRegex = Regex("""^@\w{5,32}$""")
-        private val emailRegex = Regex("""^[A-Za-z0-9_+-]+(\.[A-Za-z0-9_+-]+)*@[A-Za-z0-9-]+(\.[A-Za-z0-9-]+)+$""")
-        fun isValidSurnameAndInitials(value: String) = surnameWithInitialsRegex.matches(value)
-        fun isValidGit(value: String?) = value == null || gitRegex.matches(value)
+        private val surnameWithInitialsRegex = Regex("""^[\p{L}-]+ \p{L}\.( \p{L}\.)?$""")
+
+        fun isValidSurnameWithInitials(value: String) = surnameWithInitialsRegex.matches(value)
         fun isValidContact(value: String?) = value == null || phoneRegex.matches(value) || telegramRegex.matches(value) || emailRegex.matches(value)
     }
-    val id: Int
-    val surnameAndInitials: String
-    val git: String?
-    val contact: String?
+    override val id: Int
+    var surnameWithInitials: String
+        get() = field
+    override var git: String?
+        get() = field
+    var contact: String?
+        get() = field
     constructor(id: Int, info: String) {
         this.id = id
         val (initials, git, contact) = info.split(", ")
         this.surnameWithInitials = initials.drop(8)
         if (!isValidSurnameWithInitials(this.surnameWithInitials))
-            throw IllegalArgumentException("Invalid name")
+            throw IllegalArgumentException("ФИО имеет недопустимое значение")
         this.git = if (":" in git) git.drop(5) else null
-        if (!isValidGit(this.git))
-            throw IllegalArgumentException("Invalid GIT")
+        if (!CheckGit(this.git))
+            throw IllegalArgumentException("Ссылка на Git имеет недопустимое значение")
         this.contact = if (":" in contact) contact.substringAfterLast(": ") else null
         if (!isValidContact(this.contact))
-            throw IllegalArgumentException("Invalid contact")
+            throw IllegalArgumentException("Контакт имеет недопустимое значение")
     }
-    constructor(student: Student) : this(student.ID, student.getInfoSt())
+    constructor(student: Student) : this(student.id, student.getInfoSt())
     override fun toString(): String {
         var str = "[ID $id] $surnameWithInitials"
         if (git != null) str += "\nGit: $git"
         if (contact != null) str += "\nКонтакт: $contact"
         return "$str\n"
     }
-    fun show() = println(this.toString())
 }
