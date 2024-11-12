870c2ae038f184be4d6da55f3d85d584074d7092
anastasiafrolova2002 <147087434+anastasiafrolova2002@users.noreply.github.com>
2024-11-12 12:46:20 +0300

    LAB2 part2 TASK 2 - методы для класса DataTable

--- DataTable.kt
+++ DataTable.kt
@@ -1,3 +1,5 @@
 class DataTable(private val array: Array<Array<Any>>) {
-    
+    fun get(row: Int, am: Int) = array[row][am]
+    fun getRowAmount() = array.size
+    fun getColAmount() = if (array.isNotEmpty()) array[0].size else 0
 }
