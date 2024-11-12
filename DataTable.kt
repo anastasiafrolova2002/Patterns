b1a0a0df7f8da0ca3c831ab80ec0908d96659823
anastasiafrolova2002 <147087434+anastasiafrolova2002@users.noreply.github.com>
2024-11-12 13:17:46 +0300

    LAB2 part2 TASK 5-8 - изменения для применения DataListStudentShort

--- DataTable.kt
+++ DataTable.kt
@@ -1,5 +1,5 @@
-class DataTable(private val array: Array<Array<Any>>) {
-    fun get(row: Int, am: Int) = array[row][am]
+class DataTable(private val array: List<List<Any?>>) {
+    operator fun get(row: Int, col: Int) = array[row][col]
     fun getRowAmount() = array.size
     fun getColAmount() = if (array.isNotEmpty()) array[0].size else 0
 }
