8b1626f0d52c10962cd37e0e1edf99b9cef37d3d
anastasiafrolova2002 <147087434+anastasiafrolova2002@users.noreply.github.com>
2024-11-12 13:15:59 +0300

    LAB2 part2 TASK 5-8 - изменения для применения класса DataListStudentShort

--- DataList.kt
+++ DataList.kt
@@ -1,6 +1,11 @@
-abstract class DataList(private val array: List<Any>) {
+abstract class DataList(protected val array: List<Any?>) {
     private val selectedIndices: MutableList<Int> = mutableListOf()
+
+    fun select(number: Int) = selectedIndices.add(number)
+
     fun getSelected() = selectedIndices.toList()
-    abstract fun getNames()
-    abstract fun getData()
+
+    abstract fun getNames() : List<String>
+
+    abstract fun getData() : DataTable
 }
