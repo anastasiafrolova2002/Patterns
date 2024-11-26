4bf02710d387d6eb0cba801f93e5de2cd54a096a
anastasiafrolova2002 <147087434+anastasiafrolova2002@users.noreply.github.com>
2024-11-26 10:30:39 +0300

    LAB3 TL 1 - работа со списком студентов и txt файлом

--- Student.kt
+++ Student.kt
@@ -1,6 +1,3 @@
-import java.io.File
-import java.io.FileNotFoundException
-
 class Student (
     override val id: Int,
     name: String,
@@ -11,36 +8,6 @@ class Student (
     email: String? = null,
     git: String? = null
 ): SudentAbst(){
-    
-    companion object {
-        fun readFromTxt(filePath: String): List<Student> {
-            val file = File(filePath)
-            if (!file.exists()) throw FileNotFoundException("Файл '$filePath' не найден")
-            return buildList {
-                var currentLine = 1
-                for (line in file.readLines()) {
-                    if (line.isNotEmpty()) {
-                        try {
-                            add(Student(line))
-                        }
-                        catch (e: Exception) {
-                            throw Exception("Reading ERROR '$filePath', line $currentLine: ${e.message}")
-                        }
-                    }
-                    currentLine++
-                }
-            }
-        }
-        fun writeToTxt(filePath: String, students: Iterable<Student>) {
-            val file = File(filePath)
-            file.printWriter().use {
-                for (student in students) {
-                    it.println(student.toStringRow())
-                }
-            }
-        }
-    }
-    
         var name = name
         get() = field
         set(value) {
@@ -170,10 +137,10 @@ class Student (
             if (git != null) str += "\nGit: $git"
             return "$str\n"
         }
-        
-        private fun toStringRow() = listOf(
+
+        fun toStringRow() = listOf(
             id.toString(), surname, name, secondname, phone ?: "", telegram ?: "", email ?: "", git ?: "").joinToString(",")
-        
+
         fun anyGit(): Boolean {
             val result = git != null
             println("У студента $surname $name $secondname гит ${if (result) "при" else "от"}сутствует!")
@@ -192,124 +159,6 @@ class Student (
             if (hashMap.containsKey("email"))
                 email = hashMap["email"]
         }
+
+        fun copyWithChangedId(newId: Int) = Student(newId, surname, name, secondname, phone, telegram, email, git)
     }
-
-fun FileTestLab2() {
-    //read
-    val studList = Student.readFromTxt("C://Users//User//IdeaProjects//mpjjj1//src//testfile_lab2.txt")
-    studList.forEach { println(it.getInfoSt()) }
-
-    //write to
-    Student.writeToTxt("C://Users//User//IdeaProjects//mpjjj1//src//testfileOUT_lab2.txt", studList)
-    val studList2 = Student.readFromTxt("C://Users//User//IdeaProjects//mpjjj1//src//testfile_lab2.txt")
-    println()
-    studList2.forEach { println(it.getInfoSt()) }
-    require(studList2.toString() == studList.toString())
-}    
-
-
-fun part2_lb2() {
-    val studList = Student.readFromTxt("C://Users//User//IdeaProjects//mpjjj1//src//testfile_lab2.txt").map { StudentShort(it) }
-    studList.forEach { it.show() }
-    val dataList = DataListStudentShort(studList)
-    val names = dataList.getNames()
-    println(names)
-    dataList.select(0)
-    dataList.select(2)
-    val dataTable = dataList.getData()
-    for (i in 0..<dataTable.getRowAmount()) {
-        for (j in 0..<dataTable.getColAmount()) {
-            print("${dataTable[i, j]} ")
-        }
-        println()
-    }
-}
-
-fun main() {
-    val students = mutableListOf(
-
-    Student(mapOf(
-        "ID" to 1,
-        "name" to "Ivan",
-        "surname" to "Ivanovich",
-        "secondname" to "Ivanov"
-    )),
-    Student(mapOf(
-        "ID" to 2,
-        "name" to "Jack",
-        "surname" to "Jackov",
-        "secondname" to "Jacksov",
-        "phone" to "+79528459854",
-        "telegram" to "@chips_the_unlimited",
-        "email" to "jack999@mail.com",
-        "git" to "https://github.com/jack999"
-    )),
-    Student(mapOf(
-        "ID" to 3,
-        "name" to "Ann",
-        "surname" to "Anny",
-        "secondname" to "Annovich",
-        "telegram" to "@the_anna",
-        "git" to "https://github.com/ann300"
-    )),
-    Student(mapOf(
-        "ID" to 4,
-        "name" to "Are",
-        "surname" to "Who",
-        "secondname" to "You",
-        "git" to "https://gitlab.com/unknown"
-    )),
-    Student(mapOf(
-        "ID" to 5,
-        "name" to "Anastasia",
-        "surname" to "Frolova",
-        "secondname" to "Aleksandrovna",
-        "email" to "blumwinx2000@mail.com"
-    )),
-    Student(mapOf(
-        "ID" to 6,
-        "name" to "Irina",
-        "surname" to "IX",
-        "secondname" to "no secondname",
-        "email" to "lol2000@mail.com",
-        "phone" to "9999lol"
-    )),
-    )
-    students.forEach { it.show() }
-    //check contacts
-    students.forEach { it.anyGit() }
-    students.forEach { it.anyContact() }
-
-    students[1].setContacts(mapOf("telegram" to null, "email" to "newemail@com.com"))
-    students[1].show()
-    println("\nLab 2 results:\n")
-    lab2()
-}
-fun lab2() {
-    val students = mutableListOf(
-        Student("1,Фролова,Анастасия,Александровна,,,,"),
-        Student("2,Фамилия,Имя,Отчество,+79528459854,@mail_heck,ex@example.com,https://github.com/git_check"),
-        Student("3,Иванов,Иван,Иванович,,@ivan2002,,https://github.com/ivan_200002"),
-        Student("4,Surname,Name,SecondName,,,,https://gitlab.com/user"),
-        Student("5,Вишня,Олег,Петросович,,,cherry@mail.com,"),
-        //Student("5.5,Вишня2,Олег2,Петросович2,,,cherry2@mail.com,"),
-    )
-
-    students.forEach { it.show() }
-    students.forEach { println(it.getInfoSt())}
-    val StudentsSHORT = mutableListOf(
-        StudentShort(students[0]),
-        StudentShort(students[1]),
-        StudentShort(students[2]),
-        StudentShort(students[3]),
-        StudentShort(students[4]),
-        StudentShort(6, "Студент Дыня О., git не указан, контакты не указаны"),
-        StudentShort(7, "Студент Иванов Н. Н., git: https://github.com/ivan, связаться можно по номеру телефона: +79528459855")
-    )
-    StudentsSHORT.forEach { it.show() }
-    print("\nEx 7:\n")
-    FileTestLab2()
-
-    print("\nPart 2:\n")
-    part2_lb2()
-}
